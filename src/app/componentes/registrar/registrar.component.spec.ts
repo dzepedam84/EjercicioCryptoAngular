51cf69909945880d3748423b060fd7a2bf13cdd3
daniel <kenji_d69@yahoo.com.mx>
2025-08-15 14:55:33 -0600

    Prueba unitaria componente registrar

--- src/app/componentes/registrar/registrar.component.spec.ts
+++ src/app/componentes/registrar/registrar.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { RegistrarComponent } from './registrar.component';
+import { DebugElement } from '@angular/core';
+import { HttpClientModule  } from '@angular/common/http';
 
 describe('RegistrarComponent', () => {
   let component: RegistrarComponent;
   let fixture: ComponentFixture<RegistrarComponent>;
+  let el: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RegistrarComponent]
+      imports: [
+        RegistrarComponent,
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule ]
     })
     .compileComponents();
 
@@ -17,7 +24,25 @@ describe('RegistrarComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('debería crearse el componente', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('debería inicializar con el campo nombre vacío', () => {
+    const nombreControl = component.formulario.get('nombre');
+    expect(nombreControl?.value).toBe('');
+  });
+
+  it('debería marcar error si nombre tiene mas de 15 caracteres', () => {
+    const nombreControl = component.formulario.get('nombre');
+    nombreControl?.setValue('abcdefghijklmnop');
+    expect(nombreControl?.valid).toBeFalse();
+    expect(nombreControl?.errors?.['maxlength']).toBeTruthy();
+  });
+
+  it('debería ser válido con un nombre alfanumérico', () => {
+    const nombreControl = component.formulario.get('nombre');
+    nombreControl?.setValue('Juan123');
+    expect(nombreControl?.valid).toBeTrue();
   });
 });
