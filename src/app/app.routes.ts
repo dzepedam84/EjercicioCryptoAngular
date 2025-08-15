83df3b202796141a0b83b483b30aece729c5d0d1
daniel <kenji_d69@yahoo.com.mx>
2025-08-14 22:50:27 -0600

    Rutas y configuracion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { RegistrarComponent } from './componentes/registrar/registrar.component';
+import { VerificarComponent } from './componentes/verificar/verificar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'registrar', component: RegistrarComponent},
+    {path: 'verificar', component: VerificarComponent},
+    {path: '', redirectTo: 'registrar', pathMatch: 'full'},
+    {path: '**', component: RegistrarComponent}
+
+];
