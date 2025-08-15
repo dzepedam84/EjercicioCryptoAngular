bff678e3e3d07f8e91e732f091d4c76df5c4c304
daniel <kenji_d69@yahoo.com.mx>
2025-08-14 22:42:27 -0600

    Creacion componentes registrar y verificar

---
+++ src/app/componentes/verificar/verificar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verificar',
+  imports: [],
+  templateUrl: './verificar.component.html',
+  styleUrl: './verificar.component.css'
+})
+export class VerificarComponent {
+
+}
